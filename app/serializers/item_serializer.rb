e12d259e775ea0c7916c41b9f5c98f85931f0e6c
jolavb <julianb@bu.edu>
2017-11-26 20:36:39 -0500

    Add one to many relationship to serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :title, :quantity, :unit, :price, :location, :start_date, :end_date, :image
+  belongs_to :store
 end
--- app/serializers/store_serializer.rb
+++ app/serializers/store_serializer.rb
@@ -1,3 +1,4 @@
 class StoreSerializer < ActiveModel::Serializer
   attributes :id, :lat, :lng, :location, :name
+  has_many :items
 end
