275ef67b25cce6daefec251e0e4c270dd4c0591a
jolavb <julianb@bu.edu>
2018-01-02 16:09:47 -0500

    Successfully use spoontacular api to catagorize item

--- db/spoontacular.rb
+++ db/spoontacular.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
-require 'net/http'
+require 'unirest'
+# set item to title of item 9647 (lamb chops in this case)
+item = Item.find(9647).title
 
-item = Item.find(9647)
+response = Unirest.post "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/products/classify",
+  headers:{
+    "X-Mashape-Key" => "602U60IFrTmsh6r6HNxhewkk0lnUp1TnWOLjsnM3Uu3HqL2vdF",
+    "Content-Type" => "application/json",
+    "Accept" => "application/json"
+  },
+  parameters:  "{\"title\":\"#{item}\",\"upc\":\"\",\"plu_code\":\"\"}"
 
-puts item.title
+puts response.body
